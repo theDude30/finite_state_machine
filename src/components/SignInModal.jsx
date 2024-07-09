56e5543dc66d8de042aecc1f649d51a2f99e74ca
tbergman <tbergman@ebay.com>
2024-07-09 20:22:03 +0300

    fixed antd in signin modal

--- src/components/SignInModal.jsx
+++ src/components/SignInModal.jsx
@@ -1,14 +1,16 @@
 
 import React from 'react';
-import {  Modal } from 'antd';
+import {  Modal,Input } from 'antd';
 import { UserContext } from './UserContext';
+
+
 
 
 export default function SignInModal ({handleOk,handleCancel}) {
       const { user, setUser } = React.useContext(UserContext);
     const nameRef=React.useRef();
     const handleSubmit= () =>{
-        setUser(nameRef.current.value)
+        setUser(nameRef.current.input.value)
         handleOk();
     }
 
@@ -16,12 +18,12 @@ export default function SignInModal ({handleOk,handleCancel}) {
       <Modal title="Sign In" open onOk={handleSubmit} onCancel={handleCancel} >
         <p>Enter your email and password to access your account.</p>
         <div>
-            <span style={{marginRight:32}}>name</span>
-            <input type='text' ref={nameRef} style={{paddindRight:10}}/>
+            <span >name</span>
+            <Input type='text' ref={nameRef} style={{paddindRight:10}}/>
         </div>
         <div>
-            <span style={{marginRight:6}}>Password</span>
-            <input type='password' />
+            <span >Password</span>
+            <Input type='password' />
         </div>
       </Modal>
   );
